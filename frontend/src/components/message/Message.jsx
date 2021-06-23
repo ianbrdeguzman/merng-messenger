9659e841185a3e41d994393f27dc3126e6f46bae
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-06-23 14:48:31 -0400

    styled reactions

--- frontend/src/components/message/Message.jsx
+++ frontend/src/components/message/Message.jsx
@@ -3,9 +3,14 @@ import { AuthContext } from '../../context/authContext';
 import { UserContext } from '../../context/userContext';
 import './message.scss';
 import moment from 'moment';
+import Reactions from '../reactions/Reactions';
+import useShow from '../../hooks/useShow';
 
-const Message = ({ _id, content, createdAt, from }) => {
+const Message = ({ _id, content, createdAt, from, to }) => {
     const [showTooltip, setShowTooltip] = useState(false);
+    const [showReaction, setShowReaction] = useState(false);
+
+    const { show } = useShow();
 
     const {
         user: { username: loggedUser },
@@ -17,6 +22,8 @@ const Message = ({ _id, content, createdAt, from }) => {
         <div
             key={_id}
             className={`message ${from === loggedUser ? 'right' : 'left'}`}
+            onMouseOver={() => setShowReaction(true)}
+            onMouseLeave={() => setShowReaction(false)}
         >
             {from !== loggedUser && (
                 <img
@@ -24,17 +31,25 @@ const Message = ({ _id, content, createdAt, from }) => {
                     alt={selectedUser?.username}
                 />
             )}
+            {showReaction && (
+                <Reactions
+                    user={from}
+                    id={_id}
+                    position={from === loggedUser ? 'right' : 'left'}
+                />
+            )}
             <p
                 onMouseOver={() => setShowTooltip(true)}
                 onMouseLeave={() => setShowTooltip(false)}
             >
                 {content}
-                {showTooltip && (
-                    <span>
+                {showTooltip && show && (
+                    <span className='message__tooltip'>
                         {moment(+createdAt).format('MMMM DD, YYYY @ h:mm a')}
                     </span>
                 )}
             </p>
+            {showReaction && <Reactions user={to} id={_id} />}
         </div>
     );
 };
---
+++ frontend/src/components/reactions/Reactions.jsx
@@ -0,0 +1,52 @@
+import React, { useContext, useState } from 'react';
+import './reactions.scss';
+import { FiSmile } from 'react-icons/fi';
+import { AuthContext } from '../../context/authContext';
+import useShow from '../../hooks/useShow';
+
+const reactions = ['❤️', '😆', '😯', '😢', '😡', '👍', '👎'];
+
+const Reactions = ({ user, id, position }) => {
+    const [showReactions, setShowReactions] = useState(false);
+    const { show } = useShow();
+
+    const {
+        user: { username: loggedUser },
+    } = useContext(AuthContext);
+
+    const handleReactionOnClick = (content) => {
+        console.log(`Message Id: ${id}, Content: ${content}`);
+    };
+
+    return (
+        user === loggedUser && (
+            <div className='reactions'>
+                <button onClick={() => setShowReactions(!showReactions)}>
+                    <FiSmile />
+                </button>
+                {showReactions && (
+                    <ul
+                        className={`${!show ? 'small' : null} ${
+                            position === 'right' ? 'right' : 'left'
+                        }`}
+                    >
+                        {reactions.map((reaction, i) => {
+                            return (
+                                <li
+                                    key={i}
+                                    onClick={() =>
+                                        handleReactionOnClick(reaction)
+                                    }
+                                >
+                                    {reaction}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                )}
+            </div>
+        )
+    );
+};
+
+export default Reactions;
